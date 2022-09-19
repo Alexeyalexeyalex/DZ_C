33a57e0cfd9754d0d3333139117ce0d06d969afc
Alexey Silaenkov <silaenckov2014@yandex.ru>
2022-09-19 13:53:38 +0300

    add task2-4

---
+++ DZ_1seminar/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+using static System.Console;
+
+Clear();
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+Write("Введите третье число: ");
+int c = int.Parse(ReadLine()!);
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+
+Write($"max = {max}");
---
+++ DZ_1seminar/Task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int N = int.Parse(ReadLine()!);
+int i = 0;
+
+if (N > 0)
+{
+    while (i < N)
+    {
+        i++;
+        if (i % 2 == 0) Write($"{i} ");
+    }
+}
+else
+{
+    while (i > N)
+    {
+        i--;
+        if (i % 2 == 0) Write($"{i} ");
+    }
+
+}
+
+
+
