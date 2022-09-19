5294b88b2259379a1740ec5c11b5ffe45e54acf1
Alexey Silaenkov <silaenckov2014@yandex.ru>
2022-09-19 12:05:50 +0300

    init commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+int max = a;
+
+if (b>a) max=b;
+
+WriteLine($"max = {max}");
