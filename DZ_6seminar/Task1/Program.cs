413ba71dfb74c8d169d5f76399ef5173c1517194
Alexey Silaenkov <silaenckov2014@yandex.ru>
2022-10-07 19:39:43 +0300

    DZ6

---
+++ DZ_6seminar/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите числа:");
+string[] inputArray = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+WriteLine(getSumEven(inputArray));
+
+
+
+
+int getSumEven (string[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += Convert.ToInt32(array[i])>0? 1:0;
+    }
+    return result;
+}
---
+++ DZ_6seminar/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите данные b1, k1, b2 и k2:");
+string[] inputArray = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+getXY(inputArray);
+
+
+
+
+void getXY(string[] array)
+{
+    double b1 = Convert.ToDouble(array[0]);
+    double k1 = Convert.ToDouble(array[1]);
+    double b2 = Convert.ToDouble(array[2]);
+    double k2 = Convert.ToDouble(array[3]);
+    
+    double x = -(b1-b2)/(k1-k2); 
+    double y = k2*x+b2;
+    
+    WriteLine($"({x}; {y})");
+
+}
