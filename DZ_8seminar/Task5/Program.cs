905838dc4ea6b9ed68aae07492ace277f70d7701
Alexeyalexeyalex <34206586+Alexeyalexeyalex@users.noreply.github.com>
2022-10-13 18:04:07 +0300

    add dz8 task5

--- DZ_8seminar/Task5/Program.cs
+++ DZ_8seminar/Task5/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+using static System.Console;
+using Lib;
+
+Clear();
+int row = 4;
+int column = 4;
+
+MyArray.PrintMatrix(MyArray.CreateSpiralMatrix(row:row,column:column));
--- Lib/Class1.cs
+++ Lib/Class1.cs
@@ -25,7 +25,7 @@ public class MyArray
     {
         int[,] resultArray = new int[row, column];
         Random rnd = new Random();
-        for (int i = 0; i < row ; i++)
+        for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < column; j++)
             {
@@ -44,7 +44,7 @@ public class MyArray
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                Write($"{matrix[i,j]} ");
+                Write($"{matrix[i, j]} ");
             }
             WriteLine();
         }
@@ -58,14 +58,14 @@ public class MyArray
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                Write($"{matrix[i,j]} ");
+                Write($"{matrix[i, j]} ");
             }
             WriteLine();
         }
     }
     public static int[] GetArrayFromString(string inputValue)
     {
-        string[] stringValue = inputValue.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+        string[] stringValue = inputValue.Split(" ", StringSplitOptions.RemoveEmptyEntries);
         int[] intValue = new int[stringValue.Length];
         for (int i = 0; i < intValue.Length; i++)
         {
@@ -73,4 +73,133 @@ public class MyArray
         }
         return intValue;
     }
+
+    public static int[,] CreateSpiralMatrix(int row, int column)
+    {
+        int[,] result = new int[row, column];
+        int num = 1, n = 0, i = 0, j = 0,m=1;
+        result[0,0] = num;
+        while (m != row*column)
+        {
+            while (n != 3)
+            {
+                if (j < column - 1 && result[i, j + 1] == 0)
+                {
+                    j++;
+                    num++;
+                    if (result[i, j] == 0)
+                    {
+                        result[i, j] = num;
+                    }
+                    continue;
+                }
+                else n = 3;
+            }
+
+            while (n != 4)
+            {
+                if (i < row - 1 && result[i + 1, j] == 0)
+                {
+                    i++;
+                    num++;
+                    if (result[i, j] == 0)
+                    {
+                        result[i, j] = num;
+                    }
+                    continue;
+                }
+                else n = 4;
+            }
+            
+            while (n != 5)
+            {
+                if (j > 0 && result[i, j - 1] == 0)
+                {
+                    j--;
+                    num++;
+                    if (result[i, j] == 0)
+                    {
+                        result[i, j] = num;
+                    }
+                    continue;
+                }
+                else n = 5;
+            }
+            while (n != 6)
+            {
+                if (i > 0 && result[i - 1, j] == 0)
+                {
+                    i--;
+                    num++;
+                    if (result[i, j] == 0)
+                    {
+                        result[i, j] = num;
+                    }
+                    continue;
+                }
+                else n = 6;
+            }
+            m ++;
+        }
+
+
+
+
+
+
+
+
+
+
+        // int num = 1, i = 0, j = 1, a = column - 2, b = row - 2, columnI = 0,d = 0;
+
+
+        // for (int n = 0; n < 2; n++)
+        // {
+        //     for (; i < column; i++)
+        //     {
+        //         result[columnI, i] = num;
+        //         num++;
+        //     }
+        //     for (; j < row; j++)
+        //     {
+        //         result[j, column - 1] = num;
+        //         num++;
+        //     }
+        //     for (; a >= columnI; a--)
+        //     {
+        //         result[column - 1, a] = num;
+        //         num++;
+        //     }
+        //     for (; b != columnI; b--)
+        //     {
+        //         result[b, columnI] = num;
+        //         num++;
+        //     }
+        //     column -= 1;
+        //     row -= 1;
+        //     d++;
+        //     i=d;
+        //     j=d;
+        //     a=d;
+        //     b=d;
+        //     columnI ++;
+
+
+        // for (int i = 1; i < column-1; i++)
+        // {
+        //     result[1, i] = num;
+        //     num++;
+        // }
+
+        // }
+
+
+
+        return result;
+    }
 }
+
+
+
+
